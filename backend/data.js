197a34b9443c4d8fe06f608d17098006104fcbcd
JohnTLovato <jtlovato@gmail.com>
2021-02-02 16:17:47 -0700

    added cart and mongodb

--- backend/data.js
+++ backend/data.js
@@ -1,4 +1,21 @@
+import bcrypt from 'bcryptjs';
+
 const data = {
+
+  users: [
+    {
+      name: 'John',
+      email: 'admin@example.com',
+      password: bcrypt.hashSync('1234', 8),
+      isAdmin: true,
+    },
+    {
+      name: 'Jay',
+      email: 'user@example.com',
+      password: bcrypt.hashSync('1234', 8),
+      isAdmin: false,
+    },
+  ],
     products: [
       {
         _id: '1',
