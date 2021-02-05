de0a13ca3da97eab9eda330a4e70d260222be828
JohnTLovato <jtlovato@gmail.com>
2021-02-05 12:58:50 -0700

    updated again

--- backend/server.js
+++ backend/server.js
@@ -30,9 +30,9 @@ const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 app.use(express.static(path.join(__dirname, '/front_end/build')));
 app.get('*', (req, res) => res.sendFile(path.join(__dirname, '/front_end/build/index.html')))
-app.get('/', (req, res) => {
-  res.send('Server is ready');
-});
+// app.get('/', (req, res) => {
+//   res.send('Server is ready');
+// });
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
 });
