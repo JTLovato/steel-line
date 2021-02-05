bc1abd74960fbaa8182a88ccb308114f38e13b31
JohnTLovato <jtlovato@gmail.com>
2021-02-04 18:14:54 -0700

    updated
    added delivered options

--- front_end/src/screens/OrderScreen.js
+++ front_end/src/screens/OrderScreen.js
@@ -3,22 +3,33 @@ import { PayPalButton } from 'react-paypal-button-v2';
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { detailsOrder, payOrder } from '../actions/orderActions';
+import { deliverOrder, detailsOrder, payOrder } from '../actions/orderActions';
 import LoadingBox from '../components/LoadingBox';
 import MessageBox from '../components/MessageBox';
-import { ORDER_PAY_RESET } from '../constants/orderConstants';
+import {
+    ORDER_DELIVER_RESET,
+    ORDER_PAY_RESET,
+  } from '../constants/orderConstants';
 
 export default function OrderScreen(props) {
   const orderId = props.match.params.id;
   const [sdkReady, setSdkReady] = useState(false);
   const orderDetails = useSelector((state) => state.orderDetails);
-  
+  const userSignin = useSelector((state) => state.userSignin);
+  const { userInfo } = userSignin;
   const orderPay = useSelector((state) => state.orderPay);
   const {
     loading: loadingPay,
     error: errorPay,
     success: successPay,
   } = orderPay;
+
+  const orderDeliver = useSelector((state) => state.orderDeliver);
+  const {
+    loading: loadingDeliver,
+    error: errorDeliver,
+    success: successDeliver,
+  } = orderDeliver;
 
   const { order, loading, error } = orderDetails;
   const dispatch = useDispatch();
@@ -35,8 +46,14 @@ export default function OrderScreen(props) {
         };
         document.body.appendChild(script);
       };
-    if (!order || successPay || (order && order._id !== orderId)) {
+      if (
+        !order ||
+        successPay ||
+        successDeliver ||
+        (order && order._id !== orderId)
+      ) {
       dispatch({ type: ORDER_PAY_RESET });
+      dispatch({ type: ORDER_DELIVER_RESET });
         dispatch(detailsOrder(orderId));
       } else {
         if (!order.isPaid) {
@@ -47,11 +64,15 @@ export default function OrderScreen(props) {
           }
         }
       }
-  }, [dispatch, order, orderId, sdkReady, successPay]);
+    }, [dispatch, order, orderId, sdkReady, successPay, successDeliver]);
   
   const successPaymentHandler = (paymentResult) => {
     dispatch(payOrder(order, paymentResult));
     };
+
+    const deliverHandler = () => {
+        dispatch(deliverOrder(order._id));
+      };
   
   return loading ? (
     <LoadingBox></LoadingBox>
@@ -179,6 +200,21 @@ export default function OrderScreen(props) {
                     ></PayPalButton>
                   </>
                   )}
+                </li>
+              )}
+                            {userInfo.isAdmin && order.isPaid && !order.isDelivered && (
+                <li>
+                  {loadingDeliver && <LoadingBox></LoadingBox>}
+                  {errorDeliver && (
+                    <MessageBox variant="danger">{errorDeliver}</MessageBox>
+                  )}
+                  <button
+                    type="button"
+                    className="primary block"
+                    onClick={deliverHandler}
+                  >
+                    Deliver Order
+                  </button>
                 </li>
               )}
             </ul>
--- front_end/src/store.js
+++ front_end/src/store.js
@@ -4,6 +4,7 @@ import { cartReducer } from './reducers/cartReducers';
 import {
   orderCreateReducer,
   orderDetailsReducer,
+  orderDeliverReducer,
   orderDeleteReducer,
   orderListReducer,
   orderPayReducer,
@@ -58,6 +59,7 @@ const initialState = {
     productDelete: productDeleteReducer,
     orderList: orderListReducer,
     orderDelete: orderDeleteReducer,
+    orderDeliver: orderDeliverReducer,
   });
   const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
   const store = createStore(
