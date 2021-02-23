3030cd1553a16fe47616636b3ecd6c1b5ea58709
JohnTLovato <jtlovato@gmail.com>
2021-02-23 12:37:46 -0700

    fixed path names

--- backend/server.js
+++ backend/server.js
@@ -28,9 +28,9 @@ app.get('/api/config/paypal', (req, res) => {
 
 const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
-app.use(express.static(path.join(__dirname, '/frontend/build')));
+app.use(express.static(path.join(__dirname, '/front_end/build')));
 app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
+  res.sendFile(path.join(__dirname, '/front_end/build/index.html'))
 );
 // app.get('/', (req, res) => {
 //   res.send('Server is ready');
