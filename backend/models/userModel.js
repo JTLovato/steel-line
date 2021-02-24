aaa4f9eebbbc8cf294679f9865153fa64d506f7a
JohnTLovato <jtlovato@gmail.com>
2021-02-24 13:08:25 -0700

    fixed users

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -6,7 +6,6 @@ const userSchema = new mongoose.Schema(
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     isAdmin: { type: Boolean, default: false, required: true },
-    isAuth: { type: Boolean, default: true, required: true },
   },
   {
     timestamps: true,
