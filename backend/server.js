235eff8b6d5997e83a732d589457f1b6e1075ea6
JohnTLovato <jtlovato@gmail.com>
2021-02-23 17:20:16 -0700

    updated name

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/steel-line', {
+mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/shop-steel-line', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
